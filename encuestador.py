9fac8844b37a9eacb03931b54d2d26ebdaba3a5d
igponce <igponce@gmail.com>
2019-03-31 23:50:55 +0200

    Layout casi terminado. TODO: Bucle encuesta

--- encuestador.py
+++ encuestador.py
@@ -17,7 +17,7 @@ cuestiones = [
      },
 
     { "bloque": "Machismo",
-       "titulo": "Qué porcentaje de personas de tu entorno y características crees que opinan que",
+       "titulo": "Qué porcentaje de personas de tu entorno y características\n crees que opinan que",
        "tipo": "porcentaje",
        "preguntas": [
           {"tipo":'-', "titulo": "El problema del paro en España se debe a la incorporación de la mujer al mercado laboral"},
@@ -30,7 +30,7 @@ cuestiones = [
     },
 
      { "bloque": "Alcohol y drogas.",
-       "titulo": "Qué porcentaje de personas de tu entorno y características crees que opinan que",
+       "titulo": "Qué porcentaje de personas de tu entorno y características\n crees que opinan que",
        "tipo": "porcentaje",
        "preguntas": [
           {"tipo":'?', "titulo": "El alcohol no es una droga tan mala como el resto.", "alt":"El Alcohol tiene efectos tan adversos como otras drogas"},
@@ -58,90 +58,68 @@ import tkinter as tk
 
 class Application(tk.Frame):
 
-    appState = {  }
-    respuestas = []
+   appState = {  }
+   respuestas = []
 
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()    
-        self.appState["question"] = 0
+   def __init__(self, master=None):
+      super().__init__(master)
+      self.master = master
+      self.pack()
+      self.create_widgets()    
+      self.appState["question"] = 0
 
-    def crea_pantallas_encuesta(self, cuestiones, num):
+   def pantalla_encuesta(self, encuesta, num):
 
-        cc = cuestiones[num]
+      cc = cuestiones[num]
 
-        #self.titulo = tk.Label(self, master=None, cnf={"height": 2})
-        #self.titulo[text] = cc['titulo']
+      #self.titulo = tk.Label(self, master=None, cnf={"height": 2})
+      #self.titulo[text] = cc['titulo']
 
-        self.cuestion= tk.Button(self)
-        self.cuestion['text'] = cc['titulo']
-        if (cc['tipo']=='opciones') :
-            for op in cc['opciones']:
-                self.radio_button = "asd" 
+      self.cuestion= tk.Button(self)
+      self.cuestion['text'] = cc['titulo']
+      if (cc['tipo']=='opciones') :
+         for op in cc['opciones']:
+               self.radio_button = "asd" 
 
 
-    def create_widgets(self):
+   def create_widgets(self):
 
-        v = -1
+      v = -1
 
-        self.titulo = tk.Label(self, height=4, font=("Helvetica", 12) )
-        self.titulo["text"] = cuestiones[1]['titulo']
-        self.titulo.grid(column=1, row=1, columnspan=20)
+      self.titulo = tk.Label(self, height=4, font=("Helvetica", 12) )
+      self.titulo["text"] = cuestiones[1]['titulo']
+      self.titulo.grid(column=1, row=1, columnspan=20)
 
-        for i in range(0,11):
-           self.pct0 = tk.Radiobutton(self, text="{}%".format(10*i), variable=v, value=i, indicatoron=0)
-           self.pct0.grid(column=1+i, row=2)
-           # self.pct0.pack(side="left")
-           tk.Radiobutton(self,text="No sabe\nNo desea contestar", variable=v, value=i, indicatoron=0).grid(column=11, row=2)
-
-        gridLabelSpan = [
-            ('nadie', 1),
-            ('casi nadie', 2),
-            ('algunas personas', 2),
-            ('la mitad', 1),
-            ('la mayoria', 2),
-            ('todos', 1)
+      # Opciones preguntas y columnspan de cada pregunta
+      descripciones_preguntas = [
+         ('nadie', 1),
+         ('casi nadie', 2),
+         ('algunas personas\npero pocas', 2),git
+         ('la mitad', 1),
+         ('la mayoria pero\nno todos', 2),
+         ('casi todos',2),
+         ('todos', 1),
+         ('',1)
         ]
+        
+      column = 1
+      for (descr,colspan) in descripciones_preguntas:
+         tk.Label(self, text=descr).grid(row = 4,column=column, columnspan = colspan, sticky="EW")
+         column += colspan 
+
+      for i in range(0,11):
+         self.pct0 = tk.Radiobutton(self, text="{}%".format(10*i), variable=v, value=i, indicatoron=0, width=8)
+         self.pct0.grid(column=1+i, row=2, sticky="EW")
+         # self.pct0.pack(side="left")
+
+      tk.Radiobutton(self,text="No sabe\nNo desea contestar", variable=v, value=-1, indicatoron=0).grid(column=14, row=2, sticky="EW")
+
+      self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy).grid(row=7, column=4,columnspan=7, sticky="EW")
+      self.news = tk.Button(self, text="Siguiente >>").grid(row=6, column=5, sticky="EW") 
 
 
-        tk.Label(self,text="(nadie)").grid(row=3, column=1)
-        tk.Label(self,text="(casi nadie)").grid(row=3,column=2, columnspan=2)
-        tk.Label(self,text="(algunas personas, pero pocas)").grid(row=3,column=4, columnspan=2)
-        tk.Label(self,text="(la mitad)").grid(row=3,column=6)
-        tk.Label(self,text="(la mayoría, pero no todos)").grid(row=3,column=7, columnspan=2)
-        tk.Label(self,text="(casi todos)").grid(row=3,column=9)
-        tk.Label(self,text="(todos)").grid(row=3,column=10)
-
-
-
-
-        if True :
-          return
-
-
-
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(anchor="w")
-
-        self.hola = tk.Button(self)
-        self.hola["text"] = "Hola que pasa contigo tio"
-        self.hola.pack(side="bottom")
-
-        self.caption = tk.Label(self)
-        self.caption["text"]= "Lorem ipsum dolor asd asd a euuug  es y no va a aser facil ecribit tanto tiempo seguido..." 
-        self.caption.pack(side="bottom")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+if __name__ == "__main__":
+   root = tk.Tk()
+   app = Application(master=root)
+   app.mainloop()
+   pass
